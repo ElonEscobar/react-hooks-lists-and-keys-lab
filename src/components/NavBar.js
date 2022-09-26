5d239bf87530a732f5dce5bb5f0fcedd1a02aa1c
Dan Muendo <danwalker254254@gmail.com>
2022-09-26 15:38:51 -0400

    Assignment done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,16 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const listLinks = links.map((link) =>
+    <a href={`#`+link} key={link.toString()}>
+      {link}
+    </a>  
+  )
+  return (
+    <nav>
+      {listLinks}
+    </nav>
+  );
 }
 
 export default NavBar;
